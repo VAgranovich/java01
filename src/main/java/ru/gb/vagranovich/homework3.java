a8c5f1a60f98c208e743fc18f18390b06492fb42
Vagranovich <vadim.agranovich@gmail.com>
2022-04-22 22:40:07 +0300

    HomeWork 3

--- src/main/java/ru/gb/vagranovich/homework3.java
+++ src/main/java/ru/gb/vagranovich/homework3.java
@@ -1,8 +1,34 @@
 package ru.gb.vagranovich;
 
+import java.util.Arrays;
+
 public class homework3 {
 
+    public static void task1() {
+        int [] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void task2() {
+        int [] arr = new int[100];
+
+        for (int i = 0; i < arr.length;i++) {
+            arr[i] = i+1;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
     public static void main(String[] args) {
+        task1();
+        task2();
 
     }
 
