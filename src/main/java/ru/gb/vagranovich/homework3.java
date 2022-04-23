b9c6331fce2088dc51e07091ffd8b68ae815ee84
Vagranovich <vadim.agranovich@gmail.com>
2022-04-23 23:14:59 +0300

    HomeWork 3

--- src/main/java/ru/gb/vagranovich/homework3.java
+++ src/main/java/ru/gb/vagranovich/homework3.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class homework3 {
 
     public static void task1() {
-        int [] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         System.out.println(Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1) {
@@ -18,18 +18,30 @@ public class homework3 {
     }
 
     public static void task2() {
-        int [] arr = new int[100];
+        int[] arr = new int[100];
 
-        for (int i = 0; i < arr.length;i++) {
-            arr[i] = i+1;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
         }
         System.out.println(Arrays.toString(arr));
     }
 
+    public static void task3() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] = arr[i] * 2;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+
     public static void main(String[] args) {
         task1();
         task2();
-
+        task3();
     }
 
 }
