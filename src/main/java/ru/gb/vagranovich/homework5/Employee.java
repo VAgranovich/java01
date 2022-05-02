47da74aac7b8622bcf8e3d21912cb71934ac58f4
Vagranovich <vadim.agranovich@gmail.com>
2022-05-02 12:03:49 +0300

    HomeWork 4 - ShowBoard if lose

---
+++ src/main/java/ru/gb/vagranovich/homework5/Employee.java
@@ -0,0 +1,31 @@
+package ru.gb.vagranovich.homework5;
+
+public class Employee {
+    private final String fio;
+    private final String position;
+    private final String email;
+    private final String phone;
+    private final int salary;
+    private final int age;
+
+    public Employee(String fio, String position, String email, String phone, int salary, int age) {
+        this.fio = fio;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+
+
+    }
+
+    public void showEmployee() {
+        System.out.println("ФИО " + this.fio);
+        System.out.println("Должность " + this.position);
+        System.out.println("email " + this.email);
+        System.out.println("телефон " + this.phone);
+        System.out.println("Зарплата " + this.salary);
+        System.out.println("Возраст " + this.age);
+    }
+
+}
---
+++ src/main/java/ru/gb/vagranovich/homework5/homework5.java
@@ -0,0 +1,8 @@
+package ru.gb.vagranovich.homework5;
+
+public class homework5 {
+
+    public static void main(String[] args) {
+
+    }
+}
