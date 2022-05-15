26b515dc823c397b241544b1ae3a05dd62442ff3
Vagranovich <vadim.agranovich@gmail.com>
2022-05-15 21:47:34 +0300

    HomeWork 7

---
+++ src/main/java/ru/gb/vagranovich/homework7/Plate.java
@@ -0,0 +1,41 @@
+package ru.gb.vagranovich.homework7;
+
+public class Plate {
+    private int food;
+    private boolean lackOfFood = false;
+
+    //1. Сделать так, чтобы в тарелке с едой не могло получиться отрицательного количества еды
+    //(например, в миске 10 еды, а кот пытается покушать 15-20).
+
+    public Plate(int food) {
+        this.food = Math.max(food, 0);
+    }
+
+    public void decreaseFood(int n) {
+        if (food >= n) {
+            food -= n;
+        } else {
+            lackOfFood = true;
+        }
+    }
+
+    public void increaseFood(int n) {
+        if (n>0) {
+            food += n;
+            lackOfFood = false;
+        }
+    }
+
+    public boolean plateStatus() {
+        return lackOfFood;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+
+}
